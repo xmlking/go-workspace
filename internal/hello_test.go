fe075566793ed31931e1fc837c0cd693dc4f39da
Sumanth Chinthagunta <xmlking@gmail.com>
2021-12-25 09:34:53 -0800

    test(fuzz): adding FuzzMinMax fuzz tests on generaics

--- internal/hello_test.go
+++ internal/hello_test.go
@@ -1,11 +1,27 @@
 package internal
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestMax(t *testing.T) {
-	fmt.Println(Max(1, 5))
-	fmt.Println(Min(1, 3))
+func TestMinMax(t *testing.T) {
+	max := Max(1, 5)
+	min := Min(5, 1)
+	if min > max {
+		t.Errorf("min: %v shoud be less then max: %v", min, max)
+	}
+}
+
+func FuzzMinMax(f *testing.F) {
+	// Seed the initial corpus
+	f.Add(78, 66)
+
+	// Run the fuzz test
+	f.Fuzz(func(t *testing.T, a int, b int) {
+		min := Min(1, 3)
+		max := Max(1, 3)
+		if min > max {
+			t.Errorf("min: %v shoud be less then max: %v", min, max)
+		}
+	})
 }
